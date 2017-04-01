372cb5ece730209a12193644d717269ab251e765
RobbieDeegan <rdeegan10@gmail.com>
2017-04-01 16:57:36 +0100

    SQLite Database setup added

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController , Platform} from 'ionic-angular';
+import {SQLite} from "ionic-native";
 
 @Component({
   selector: 'page-about',
@@ -8,8 +9,39 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  public database: SQLite;
+    public stats: Array<Object>;
+ 
+    constructor(private navController: NavController, private platform: Platform) {
+        this.platform.ready().then(() => {
+            this.database = new SQLite();
+            this.database.openDatabase({name: "stats.db", location: "default"}).then(() => {
+                this.refresh();
+            }, (error) => {
+                console.log("ERROR: ", error);
+            });
+        });
+    }
+ 
+    public add() {
+        this.database.executeSql("INSERT INTO stats (games, goals, assists, minutes, yellow, red) VALUES (?, ?, ?, ?, ?, ?)", []).then((data) => {
+            console.log("INSERTED: " + JSON.stringify(data));
+        }, (error) => {
+            console.log("ERROR: " + JSON.stringify(error.err));
+        });
+    }
 
-  }
+     public refresh() {
+        this.database.executeSql("SELECT * FROM stats", []).then((data) => {
+            this.stats = [];
+            if(data.rows.length > 0) {
+                for(var i = 0; i < data.rows.length; i++) {
+                    this.stats.push({firstname: data.rows.item(i).firstname, lastname: data.rows.item(i).lastname});
+                }
+            }
+        }, (error) => {
+            console.log("ERROR: " + JSON.stringify(error));
+        });
+    }
 
 }
